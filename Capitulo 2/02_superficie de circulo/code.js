22682dd5fcdc5fb1236a330a268f37ebdb8f96b0
Rolo_27s <108833691+Rolo27s@users.noreply.github.com>
2022-12-21 12:27:36 +0100

    Add files via upload

---
+++ Capitulo 2/02_superficie de circulo/code.js
@@ -0,0 +1,33 @@
+﻿const PI = 3.141592;
+
+function solution() {
+    let n1 = document.getElementById("n1").value;
+    let n2 = document.getElementById("n2").value;
+    const sol1 = document.getElementById("sol1");
+    const sol2 = document.getElementById("sol2");
+    const diff = document.getElementById("diff");
+
+    let solution1 = PI * n1 * n1;
+    let solution2 = PI * n2 * n2;
+    let difference;
+
+    solution1 = solution1.toFixed(2);
+    solution2 = solution2.toFixed(2);
+
+    sol1.innerHTML = solution1 + "  cm²"; /* ALT + 0178 */
+    sol2.innerHTML = solution2 + "  cm²";
+
+    if (n1 > n2) {
+        difference = solution1 - solution2;
+        difference = difference.toFixed(2);
+        diff.innerHTML = difference + " cm²";
+    } else if (n2 > n1) {
+        difference = solution2 - solution1;
+        difference = difference.toFixed(2);
+        diff.innerHTML = difference + " cm²";
+    } else {
+        difference = 0;
+        difference = difference.toFixed(2);
+        diff.innerHTML = difference + " cm²";
+    }
+}
