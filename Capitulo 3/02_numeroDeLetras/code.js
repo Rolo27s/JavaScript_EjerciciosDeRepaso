d487bb244557ceb6e911c744b090aa5427093096
Rolo_27s <108833691+Rolo27s@users.noreply.github.com>
2023-01-01 12:41:24 +0100

    Add files via upload

---
+++ Capitulo 3/02_numeroDeLetras/code.js
@@ -0,0 +1,7 @@
+﻿function medir(frase){
+    /* let frase = document.getElementById("frase").value; */ /* De esta manera le puedo pasar el input con parametro */
+    let sol = document.getElementById("solucion");
+    let size = frase.length;
+    sol.innerHTML = "El contenido mide " + size;
+}
+
---
+++ Capitulo 3/03_nombreApellido_Iniciales/code.js
@@ -0,0 +1,13 @@
+﻿function generarSiglas(frase){
+    /* let frase = document.getElementById("frase").value; */ /* De esta manera le puedo pasar el input con parametro */
+    let sol = document.getElementById("solucion");
+    let size = frase.length;
+    let siglas = "";
+    for(let i = 0; i < size; i++){
+        if(i == 0) siglas = frase[i];
+        if(frase[i] == ' ') siglas += frase[i+1];
+    }
+    siglas = siglas.toUpperCase();
+    sol.innerHTML = "Las siglas son: " + siglas;
+}
+
