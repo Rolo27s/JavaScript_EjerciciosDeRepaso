dce66c0170845de8e3c3943e704bd8e6fb99c61b
Rolo_27s <108833691+Rolo27s@users.noreply.github.com>
2023-01-02 11:14:45 +0100

    Add files via upload

---
+++ Capítulo 04 - Procesamiento condicionado/01 PolinomioSegundoGrado/code.js
@@ -0,0 +1,14 @@
+﻿const sol = document.getElementById("Solucion");
+sol.innerHTML = "Prueba";
+
+function solve(a, b, c){
+    console.log("Valor de A: " + a + " |", "Valor de B: " + b + " |", "Valor de C: " + c);
+    let result = (-1 * b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+    let result2 = (-1 * b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+
+    result = result.toFixed(4);
+    result2 = result2.toFixed(4);
+
+    sol.innerHTML = result + " &nbsp y &nbsp " + result2;
+    return result + " &nbsp y &nbsp " + result2;
+}
