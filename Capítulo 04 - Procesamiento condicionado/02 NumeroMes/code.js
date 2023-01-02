04119377e2044282e8c7f34bcbc80d19b375327a
Rolo_27s <108833691+Rolo27s@users.noreply.github.com>
2023-01-02 12:05:33 +0100

    Add files via upload

---
+++ Capítulo 04 - Procesamiento condicionado/02 NumeroMes/code.js
@@ -0,0 +1,19 @@
+﻿const sol = document.getElementById("sol");
+
+function queMes(mes){
+    switch(mes){
+        case "1": sol.innerHTML = "ENERO"; break;
+        case "2": sol.innerHTML = "FEBRERO"; break;
+        case "3": sol.innerHTML = "MARZO"; break;
+        case "4": sol.innerHTML = "ABRIL"; break;
+        case "5": sol.innerHTML = "MAYO"; break;
+        case "6": sol.innerHTML = "JUNIO"; break;
+        case "7": sol.innerHTML = "JULIO"; break;
+        case "8": sol.innerHTML = "AGOSTO"; break;
+        case "9": sol.innerHTML = "SEPTIEMBRE"; break;
+        case "10": sol.innerHTML = "OCTUBRE"; break;
+        case "11": sol.innerHTML = "NOVIEMBRE"; break;
+        case "12": sol.innerHTML = "DICIEMBRE"; break;
+        default: sol.innerHTML = "Error de referencia - Elige un mes del 1 al 12"; break;
+    }
+}
