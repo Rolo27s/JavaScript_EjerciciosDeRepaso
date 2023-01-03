8e47cb057a85d5953b37e149be959db71f9cfa62
Rolo_27s <108833691+Rolo27s@users.noreply.github.com>
2023-01-03 13:34:23 +0100

    Add files via upload

---
+++ Capítulo 05 - Procesamiento iterativo bucles/02 MediaEneNumeros/code.js
@@ -0,0 +1,26 @@
+﻿/* Defino como constante el numero de numeros a evaluar y lo pido al usuario*/
+const N = parseInt(prompt("Cuantos numeros quieres calcular?: "));
+
+/* Defino n como un array de numeros */
+let n = [];
+let total = 0;
+
+for(let i = 0 ; i < N ; i++){  
+    n[i] = parseInt(prompt("Escribe un numero " + (i+1) + "/" + N + " :"));
+}
+
+/* Veo en consola el valor de mis numeros y calculo el total */
+for(let i = 0 ; i < N ; i++){
+    console.log(n[i]);
+    total += n[i];
+}
+
+console.log("TOTAL: " + total);
+
+document.write("La media del conjunto de numeros <br>");
+for(let i = 0 ; i < N ; i++){
+    if (i < N-1) document.write(n[i] + ", ");
+    else document.write(n[i] + " es: <br>");
+}
+
+document.write((total/N).toFixed(2));
---
+++ Capítulo 05 - Procesamiento iterativo bucles/03 numVocales/code.js
@@ -0,0 +1,22 @@
+﻿/* Pido una palabra al usuario */
+let palabra = prompt("Dame una palabra: ");
+
+/* Convierto la palabra a todo mayusculas para deterctar mejor las vocales */
+palabra = palabra.toUpperCase();
+
+/* Guardo el valor de la longitud de mi palabra */
+let longitud = palabra.length;
+let nV = 0;
+
+for(let i = 0 ; i < longitud ; i++){
+    switch(palabra[i]){
+        case 'A': case 'E' : case 'I': case 'O': case 'U': 
+        nV++;
+        break;
+    }
+}
+document.write("La palabra " + palabra + (" mide ") + longitud + "<br>");
+
+if (nV == 1) document.write("Tu palabra tiene " + nV + " vocal");
+if (nV > 1) document.write("Tu palabra tiene " + nV + " vocales");
+if (nV == 0) document.write("Tu palabra no tiene vocales y te la has inventado");
