476e8abb45b9248425ea5cc963ec6450b4d868db
Rolo_27s <108833691+Rolo27s@users.noreply.github.com>
2023-01-04 11:55:03 +0100

    Add files via upload

---
+++ Capítulo 07 - Procedimientos y funciones/01 funcionDoble/code.js
@@ -0,0 +1,12 @@
+﻿let n1 = parseInt(prompt("Dame un numero para multiplicarlo por dos: "));
+let n2 = parseInt(prompt("Dame otro numero para multiplicarlo por dos: "));
+
+function doble (n1, n2){
+    let n1D = n1*2;
+    let n2D = n2*2;
+
+    document.write(n1 + " x 2 = " + n1D + "<br>");
+    document.write(n2 + " x 2 = " + n2D);
+}
+
+doble (n1, n2);
---
+++ Capítulo 07 - Procedimientos y funciones/02 Maximo/code.js
@@ -0,0 +1,13 @@
+﻿let n1 = parseInt(prompt("Dame un numero: "));
+let n2 = parseInt(prompt("Dame otro numero: "));
+
+function maximo (n1, n2){
+    let max;
+    if (n1 > n2) max = n1;
+    else if (n1 < n2) max = n2;
+    else max = n1 + ", son iguales";
+
+    document.write("El numero maximo es " + max);
+}
+
+maximo (n1, n2);
